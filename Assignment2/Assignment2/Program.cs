a11faf7f5a89d3d71c98745393a57fe0e1384276
Cameron Helkuik <cameron@travefy.com>
2019-04-23 10:40:38 -0500

    Initial commit

---
+++ Assignment2/Assignment2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var continueLoop = true;
+
+            do
+            {
+                try
+                {
+                    Console.Write("Enter miles driven: ");
+                    double inputMiles = double.Parse(Console.ReadLine());
+
+                    Console.Write("Enter gallons used: ");
+                    double inputGallons = double.Parse(Console.ReadLine());
+
+                    double mpg = inputMiles / inputGallons;
+
+                    Console.WriteLine($"{mpg:F2} miles per gallon"); 
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Please enter decimal numbers for the miles driven and gallons used");
+                }
+
+                Console.Write("Quit (yes/no)? ");
+                string quitResponse = Console.ReadLine();
+
+                if (quitResponse == "yes")
+                {
+                    continueLoop = false;
+                }
+
+            } while (continueLoop);
+        }
+    }
+}
